e5913ca1ea840697ca60ce2a80cb6f84aeeaf9ab
Igor-Macbook Pro <igor.naum.2013@yandex.ru>
2019-01-30 04:20:07 +0300

    Initial Commit

---
+++ MessagerApplication/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MessagerApplication
+//
+//  Created by Igor-Macbook Pro on 30/01/2019.
+//  Copyright © 2019 Igor-Macbook Pro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
